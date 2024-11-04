b046aa7738c125cf2c20d1ff3201201cc714d4bd
8dcc <8dcc.git@gmail.com>
2024-11-04 15:44:44 +0100

    Add Package-Requires and Keywords

--- plumber.el
+++ plumber.el
@@ -2,18 +2,20 @@
 
 ;; Author: 8dcc <8dcc.git@gmail.com>
 ;; Version: 1.0.0
+;; Package-Requires: ((emacs "25.1") (compat "28.1.2.2"))
+;; Keywords: convenience, matching, tools
 ;; URL: https://github.com/8dcc/plumber.el
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
